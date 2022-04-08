3c9691fb49563f179cf078644baa0827e13ea5da
Paula Wiltiner <paulawiltiner.pw@gmail.com>
2022-04-08 10:42:10 -0300

    pedido de cartas

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function inserirCartas(qtd){
+    let contador =qtd
+    let jogo = document.querySelector(".jogo")
+    while (contador >0){
+        contador= contador-1;
+        jogo.innerHTML = jogo.innerHTML + "<div class='card'><img src='Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png' ></div>"
+    }
+}
+
+
+function start (){
+    let quantidade_cartas = prompt("Qual o número cartas que deseja ?")
+    if (isNaN(quantidade_cartas) === false){
+        Number(quantidade_cartas)
+        if( quantidade_cartas <= 14 && quantidade_cartas >= 4){
+            if (quantidade_cartas%2 === 0 ){
+                alert(" Vamos começar o jogo !")
+                inserirCartas(quantidade_cartas)
+            } else {
+                alert("Por favor, insira um número par de cartas.")
+            }
+        }else{
+            alert("Por favor, insira um número de cartas entre 4 e 14.")
+            start()
+        }
+    }else{
+        alert("Por favor, insira um número par de cartas entre 4 e 14.")
+        start()
+    }
+}
+start()
+
